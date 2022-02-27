fb22c077d479f6f9473c40f2bddcec7b49be6475
James Andariese <james@strudelline.net>
2022-02-26 22:00:48 -0600

    enhanced ergonomics

--- main.go
+++ main.go
@@ -14,65 +14,80 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"io"
 )
 
-var ErrPemUnderlyingFormatError = errors.New("pem passed was not a string or []byte")
+var ErrPemUnderlyingFormatError = errors.New("pem passed was not a string, []byte, or io.Reader")
 var ErrPemIsUnsupportedType = errors.New("pem is an unsupported type")
 
 func intoBytes(pemInt interface{}) ([]byte, error) {
-	var pemBytes []byte = nil
-	if b, ok := pemInt.([]byte); ok {
-		pemBytes = b
-	}
-	if s, ok := pemInt.(string); ok {
-		pemBytes = []byte(s)
-	}
-	if pemBytes == nil {
+	switch v := pemInt.(type) {
+	case []byte:
+		return v, nil
+	case string:
+		return []byte(v), nil
+	case io.Reader:
+		pemBytes, err := io.ReadAll(v)
+		if err != nil {
+			return nil, err
+		}
+		return pemBytes, nil
+	default:
 		return nil, ErrPemUnderlyingFormatError
 	}
-	return pemBytes, nil
+	panic("unreachable")
 }
 
 // Parsing a PEM results in a ParsedPEM object being returned
-type ParsedPEM struct {
-	obj interface{}
+type ParsedPEMs struct {
+	objs []interface{}
+}
+
+// Return the number of parsed PEMs remaining to be consumed
+func (p *ParsedPEMs) Length() int {
+	return len(p.objs)
 }
 
 // Give the object back in its typeless form
-func (p ParsedPEM) Interface() interface{} {
-	return p.obj
+func (p *ParsedPEMs) Interface() interface{} {
+	ret := p.objs[0]
+	p.objs = p.objs[1:]
+	return ret
 }
 
 // Return the ParsedPEM's object as a *x509.Certificate.
 //
 // Panics if the object wasn't an x.509 certificate
-func (p ParsedPEM) MustCertificate() *x509.Certificate {
-	r, ok := p.obj.(*x509.Certificate)
+func (p *ParsedPEMs) MustCertificate() *x509.Certificate {
+	r, ok := p.objs[0].(*x509.Certificate)
 	if !ok {
-		panic(fmt.Sprintf("%#v is not an *x509.Certificate", p.obj))
+		panic(fmt.Sprintf("%#v is not an *x509.Certificate", p.objs[0]))
 	}
+	p.objs = p.objs[1:]
 	return r
 }
 
 // Returns the ParsedPEM's object as a *rsa.PrivateKey
 //
 // Panics if the object wasn't an RSA private key
-func (p ParsedPEM) MustRSAPrivateKey() *rsa.PrivateKey {
-	r, ok := p.obj.(*rsa.PrivateKey)
+func (p *ParsedPEMs) MustRSAPrivateKey() *rsa.PrivateKey {
+	r, ok := p.objs[0].(*rsa.PrivateKey)
 	if !ok {
-		panic(fmt.Sprintf("%#v is not an rsa.PrivateKey", p.obj))
+		panic(fmt.Sprintf("%#v is not an rsa.PrivateKey", p.objs[0]))
 	}
+	p.objs = p.objs[1:]
 	return r
 }
 
 // Returns the ParsedPEM's object as a *ecdsa.PrivateKey
 //
 // Panics if the object wasn't an ECDSA private key
-func (p ParsedPEM) MustECPrivateKey() *ecdsa.PrivateKey {
-	r, ok := p.obj.(*ecdsa.PrivateKey)
+func (p *ParsedPEMs) MustECPrivateKey() *ecdsa.PrivateKey {
+	r, ok := p.objs[0].(*ecdsa.PrivateKey)
 	if !ok {
-		panic(fmt.Sprintf("%#v is not an ecdsa.PrivateKey", p.obj))
+		panic(fmt.Sprintf("%#v is not an ecdsa.PrivateKey", p.objs[0]))
 	}
+	p.objs = p.objs[1:]
 	return r
 }
 
@@ -86,11 +101,11 @@ func (p ParsedPEM) MustECPrivateKey() *ecdsa.PrivateKey {
 //
 // Produces an error if there is no PEM data found.
 //
-func ParsePEM(pemInt interface{}) ([]ParsedPEM, error) {
-	objs := []ParsedPEM{}
+func ParsePEMs(pemInt interface{}) (ParsedPEMs, error) {
+	objs := []interface{}{}
 	pemBytes, err := intoBytes(pemInt)
 	if err != nil {
-		return nil, err
+		return ParsedPEMs{}, err
 	}
 	var der *pem.Block
 	var rest []byte = pemBytes
@@ -103,32 +118,32 @@ func ParsePEM(pemInt interface{}) ([]ParsedPEM, error) {
 		case "CERTIFICATE":
 			r, err := x509.ParseCertificate(der.Bytes)
 			if err != nil {
-				return nil, err
+				return ParsedPEMs{}, err
 			}
-			objs = append(objs, ParsedPEM{r})
+			objs = append(objs, r)
 		case "RSA PRIVATE KEY":
 			r, err := x509.ParsePKCS1PrivateKey(der.Bytes)
 			if err != nil {
-				return nil, err
+				return ParsedPEMs{}, err
 			}
-			objs = append(objs, ParsedPEM{r})
+			objs = append(objs, r)
 		case "EC PRIVATE KEY":
 			r, err := x509.ParseECPrivateKey(der.Bytes)
 			if err != nil {
-				return nil, err
+				return ParsedPEMs{}, err
 			}
-			objs = append(objs, ParsedPEM{r})
+			objs = append(objs, r)
 		case "PRIVATE KEY":
 			r, err := x509.ParsePKCS8PrivateKey(der.Bytes)
 			if err != nil {
-				return nil, err
+				return ParsedPEMs{}, err
 			}
-			objs = append(objs, ParsedPEM{r})
+			objs = append(objs, r)
 		default:
 		}
 	}
 	if len(objs) > 0 {
-		return objs, nil
+		return ParsedPEMs{objs}, nil
 	}
-	return nil, ErrPemIsUnsupportedType
+	return ParsedPEMs{}, ErrPemIsUnsupportedType
 }
--- main_test.go
+++ main_test.go
@@ -9,76 +9,92 @@ import (
 	"testing"
 )
 
-//go:embed testfiles/rsakey.pem
+//go:embed testfiles/rsa_512.key
 var test_rsakey []byte
 
-//go:embed testfiles/rsareq.pem
+//go:embed testfiles/rsa_512.csr
 var test_rsareq []byte
 
-//go:embed testfiles/rsacert.pem
+//go:embed testfiles/rsa_512.crt
 var test_rsacert []byte
 
-//go:embed testfiles/ca/ca.pem
+//go:embed testfiles/ca/ca.crt
 var test_ca []byte
 
-//go:embed testfiles/ca/cakey.pem
+//go:embed testfiles/ca/ca.key
 var test_cakey []byte
 
-//go:embed testfiles/eccert.pem
+//go:embed testfiles/ec_P-521.crt
 var test_eccert []byte
 
-//go:embed testfiles/eckey.pem
+//go:embed testfiles/ec_P-521.key
 var test_eckey []byte
 
-//go:embed testfiles/*
+//go:embed testfiles/*.crt testfiles/*.key testfiles/*.csr
 var test_fs embed.FS
 
-func ExampleParsePEM() {
-	f, err := test_fs.Open("testfiles/eckey.pem")
+func ExampleParsePEMs() {
+	f, err := test_fs.Open("testfiles/ec_P-256.key")
 	if err != nil {
-		fmt.Printf("Failed to open eckey.pem: %v", err)
+		fmt.Printf("Failed to open ec_P-256.key: %v", err)
 		return
 	}
 	bytes, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Printf("Failed to read eckey.pem: %v", err)
+		fmt.Printf("Failed to read ec_P-256.key: %v", err)
 		return
 	}
-
 	var becpriv *ecdsa.PrivateKey
 	var secpriv *ecdsa.PrivateKey
+	var fecpriv *ecdsa.PrivateKey
 
 	{ // it works with []byte.
-		pemObjs, err := ParsePEM(bytes)
-		if err != nil || len(pemObjs) != 1 {
+		pemObjs, err := ParsePEMs(bytes)
+		if err != nil || pemObjs.Length() != 1 {
 			fmt.Printf("failed to parse PEM: %v", err)
 			return
 		}
-		becpriv = pemObjs[0].MustECPrivateKey()
+		becpriv = pemObjs.MustECPrivateKey()
 	}
 
 	{ // it also works with string.  magic!
-		pemObjs, err := ParsePEM(string(bytes))
-		if err != nil || len(pemObjs) != 1 {
+		pemObjs, err := ParsePEMs(string(bytes))
+		if err != nil || pemObjs.Length() != 1 {
 			fmt.Printf("failed to parse PEM: %v", err)
 			return
 		}
-		secpriv = pemObjs[0].MustECPrivateKey()
+		secpriv = pemObjs.MustECPrivateKey()
 	}
 
-	// the two should be the same.
-	if secpriv.Equal(becpriv) {
-		fmt.Println("string and byte versions are equal")
+	{ // but wait!  there's more!  it also works with io.Reader!
+		// we could have skipped the whole io.ReadAll after all!
+		// this block is all we needed.
+		f, err := test_fs.Open("testfiles/ec_P-256.key")
+		if err != nil {
+			fmt.Printf("Failed to open ec_P-256.key: %v", err)
+			return
+		}
+		pemObjs, err := ParsePEMs(f)
+		if err != nil || pemObjs.Length() != 1 {
+			fmt.Printf("failed to parse ec_P-256.key: %v", err)
+			return
+		}
+		fecpriv = pemObjs.MustECPrivateKey()
 	}
-	//Output: string and byte versions are equal
+
+	// the three should be the same.  let's just check to be sure.
+	if secpriv.Equal(becpriv) && fecpriv.Equal(becpriv) {
+		fmt.Println("string, byte, and io.Reader versions are equal")
+	}
+	//Output: string, byte, and io.Reader versions are equal
 }
 
 func TestLoadPem(t *testing.T) {
-	obj, err := ParsePEM(test_eckey)
+	objs, err := ParsePEMs(test_eckey)
 	if err != nil {
 		t.Errorf("unexpected error parsing pem %#v", err)
 	}
-	t.Logf("parsedObj: %#v", obj[0])
+	t.Logf("parsedObj: %#v", objs.Interface())
 }
 
 func TestLoadManyPem(t *testing.T) {
@@ -92,23 +108,20 @@ func TestLoadManyPem(t *testing.T) {
 		test_eckey,
 	}
 	pems := bytes.Join(pembyteblocks, []byte{'\n'})
-	objs, err := ParsePEM(pems)
+	objs, err := ParsePEMs(pems)
 	if err != nil {
 		t.Errorf("error while reading PEMs %#v", err)
 	}
-	for _, obj := range objs {
-		t.Logf("%#v", obj)
-	}
-	if len(objs) != len(pembyteblocks)-1 {
+	if objs.Length() != len(pembyteblocks)-1 {
 		// except for the csr which we don't parse and _should_ be missing
 		t.Error("ParsePEM did not parse all the expected blocks properly")
 	}
-	rsacert := objs[0].MustCertificate()
-	rsakey := objs[1].MustRSAPrivateKey()
-	cacert := objs[2].MustCertificate()
-	cakey := objs[3].MustRSAPrivateKey()
-	eccert := objs[4].MustCertificate()
-	eckey := objs[5].MustECPrivateKey()
+	rsacert := objs.MustCertificate()
+	rsakey := objs.MustRSAPrivateKey()
+	cacert := objs.MustCertificate()
+	cakey := objs.MustRSAPrivateKey()
+	eccert := objs.MustCertificate()
+	eckey := objs.MustECPrivateKey()
 	if !rsakey.PublicKey.Equal(rsacert.PublicKey) {
 		t.Error("okay...")
 	}
@@ -118,6 +131,13 @@ func TestLoadManyPem(t *testing.T) {
 	if !eckey.PublicKey.Equal(eccert.PublicKey) {
 		t.Errorf("%#v != %#v", eckey.PublicKey, eccert.PublicKey)
 	}
+}
+
+func TestLoadPemUnderrun(t *testing.T) {
+	objs, err := ParsePEMs(test_eckey)
+	if err != nil {
+		t.Errorf("unexpected error parsing pem %#v", err)
+	}
 	failedAsExpected := false
 	func() {
 		defer func() {
@@ -125,9 +145,28 @@ func TestLoadManyPem(t *testing.T) {
 				failedAsExpected = true
 			}
 		}()
-		objs[5].MustCertificate()
+		objs.Interface()
+		objs.Interface()
 	}()
 	if !failedAsExpected {
-		t.Error("Expected MustCertificate to fail for EC private key but it did not")
+		t.Error("Expected an error from object underrun but there was no panic")
+	}
+}
+func TestLoadPemWrongType(t *testing.T) {
+	objs, err := ParsePEMs(test_eckey)
+	if err != nil {
+		t.Errorf("unexpected error parsing pem %#v", err)
+	}
+	failedAsExpected := false
+	func() {
+		defer func() {
+			if recover() != nil {
+				failedAsExpected = true
+			}
+		}()
+		t.Logf("parsedObj: %#v", objs.MustRSAPrivateKey())
+	}()
+	if !failedAsExpected {
+		t.Error("Expected an error from trying to coerce an EC to RSA but there was no panic")
 	}
 }
